37f1f92dd9bffba0627145d351483578cd2be89b
zhangjq737 <zjqrich@gmail.com>
2025-01-15 09:56:52 -0500

    initialize spring project

---
+++ week1/src/main/java/com/humber/week1/Week1Application.java
@@ -0,0 +1,14 @@
+package com.humber.week1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week1Application.class, args);
+                System.out.println("Hello World 11111!");
+	}
+
+}
