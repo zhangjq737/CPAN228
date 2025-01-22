0359f9fe648fe480fd1badb6aa69b82ee0c17714
zhangjq737 <zjqrich@gmail.com>
2025-01-22 08:52:12 -0500

    initialize spring project1 1

--- week2/src/main/java/com/humber/week2/HomeController.java
+++ week2/src/main/java/com/humber/week2/HomeController.java
@@ -1,8 +1,12 @@
 package com.humber.week2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
 }
