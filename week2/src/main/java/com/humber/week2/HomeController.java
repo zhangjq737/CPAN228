069499d4ac7a41c062aa7dd1bad27c91ab2d9d79
zhangjq737 <zjqrich@gmail.com>
2025-01-22 09:18:15 -0500

    project from week2

--- week2/src/main/java/com/humber/week2/HomeController.java
+++ week2/src/main/java/com/humber/week2/HomeController.java
@@ -1,12 +1,14 @@
 package com.humber.week2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("message", "Hello World");
         return "home";
     }
 }
