bb3bfb832c3066b95abb67a65773edc7c944d871
zhangjq737 <zjqrich@gmail.com>
2025-02-19 14:49:27 -0500

    jpa

--- test1/src/main/java/com/CPAN228/test1/model/Fighter.java
+++ test1/src/main/java/com/CPAN228/test1/model/Fighter.java
@@ -4,32 +4,27 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
-//@AllArgsConstructor
+@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class Fighter {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private LocalDateTime created_at;
+    @Column(name = "createdat")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
     private String name;
-    private int damage_per_hit;
+    @Column(name = "damageperhit")
+    private int damagePerHit;
     private int health;
     private double resistance;
-    private Anime anime_from;
-
-    public Fighter(long id, LocalDateTime createdAt, String name, int damagePerHit, int health, double resistance, Anime animeFrom) {
-        this.id = id;
-        this.created_at = createdAt;
-        this.name = name;
-        this.damage_per_hit = damagePerHit;
-        this.health = health;
-        this.resistance = resistance;
-        this.anime_from = animeFrom;
-    }
+    @Column(name = "animefrom")
+    private Anime animeFrom;
 }
